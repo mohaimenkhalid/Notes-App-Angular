1b3aa2647da576db9b84c1294dae07ed7082f1d3
mohaimenkhallid <mohaimen707@gmail.com>
2021-03-13 17:04:21 +0600

    Tempating, design card & add transition

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NotesListComponent} from './pages/notes-list/notes-list.component';
+import {MainLayoutComponent} from './pages/main-layout/main-layout.component';
 
 const routes: Routes = [
-
+  { path: '', component: MainLayoutComponent, children: [
+      { path: '', component: NotesListComponent }
+    ]}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotesListComponent } from './pages/notes-list/notes-list.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NoteCardComponent } from './note-card/note-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NotesListComponent
+    NotesListComponent,
+    MainLayoutComponent,
+    NoteCardComponent
   ],
   imports: [
     BrowserModule,
