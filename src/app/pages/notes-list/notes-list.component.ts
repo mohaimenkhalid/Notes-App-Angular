56b94875ddc27aa50a211df07097804c13a18395
mohaimenkhallid <mohaimen707@gmail.com>
2021-03-15 12:21:21 +0600

    Filter functionality adds in notes list

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Note} from '../../shared/note.model';
-import { NotesService } from '../../shared/notes.service';
+import {NotesService} from '../../shared/notes.service';
 import {Router} from '@angular/router';
+import {all} from 'codelyzer/util/function';
 
 @Component({
   selector: 'app-notes-list',
@@ -10,11 +11,13 @@ import {Router} from '@angular/router';
 })
 export class NotesListComponent implements OnInit {
   notes: Note[] = new Array<Note>();
+  filteredNotes: Note[] = new Array<Note>();
 
   constructor(private notesService: NotesService, private router: Router) { }
 
   ngOnInit(): void {
       this.notes = this.notesService.getAll();
+      this.filteredNotes = this.notes;
   }
 
   deleteNote = (id: number): void => {
@@ -23,17 +26,30 @@ export class NotesListComponent implements OnInit {
   }
 
   filter = (query: string) => {
+    console.log(query);
     query = query.toLowerCase().trim();
     let allResults: Note[] = new Array<Note>();
     let terms: string[] = query.split(' ');
     terms = this.removeDuplicates(terms);
-
+    terms.forEach(element => {
+      const results = this.relevantNotes(element);
+      allResults  = [...allResults, ...results];
+    });
+    const uniqueResults = this.removeDuplicates(allResults);
+    this.filteredNotes = uniqueResults;
   }
 
-  removeDuplicates(arr: Array<any>): Array<any> {
+  removeDuplicates = (arr: Array<any>): Array<any> => {
     const uniqueResults: Set<any> = new Set<any>();
     arr.forEach(item => uniqueResults.add(item));
     return Array.from(uniqueResults);
+  }
+
+  relevantNotes = (query: string): Array<Note> => {
+    query = query.toLowerCase().trim();
+    return this.notes.filter(note => {
+      return note.body.toLowerCase().includes(query) || note.title.toLowerCase().includes(query);
+    });
   }
 
 }
