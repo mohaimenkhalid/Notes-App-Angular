c7b4a87459ab61a679a1737e1405d57bc0eb89bb
mohaimenkhallid <mohaimen707@gmail.com>
2021-03-15 15:52:07 +0600

    filter delete & show functionality changes

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {Note} from '../../shared/note.model';
 import {NotesService} from '../../shared/notes.service';
 import {Router} from '@angular/router';
-import {all} from 'codelyzer/util/function';
 
 @Component({
   selector: 'app-notes-list',
@@ -12,17 +11,21 @@ import {all} from 'codelyzer/util/function';
 export class NotesListComponent implements OnInit {
   notes: Note[] = new Array<Note>();
   filteredNotes: Note[] = new Array<Note>();
-
+  noteCount;
   constructor(private notesService: NotesService, private router: Router) { }
 
   ngOnInit(): void {
       this.notes = this.notesService.getAll();
       this.filteredNotes = this.notes;
+      this.checkNoteIsEmpty();
   }
 
-  deleteNote = (id: number): void => {
-    this.notesService.delete(id);
+  deleteNote = (note: Note): void => {
+    const noteId = this.notesService.getId(note);
+    this.notesService.delete(noteId);
     this.router.navigateByUrl('/');
+    this.filteredNotes = this.notesService.getAll();
+    this.checkNoteIsEmpty();
   }
 
   filter = (query: string) => {
@@ -36,6 +39,9 @@ export class NotesListComponent implements OnInit {
     });
     const uniqueResults = this.removeDuplicates(allResults);
     this.filteredNotes = uniqueResults;
+    this.checkNoteIsEmpty();
+
+    this.sortByRelevancy(allResults);
   }
 
   removeDuplicates = (arr: Array<any>): Array<any> => {
@@ -49,6 +55,37 @@ export class NotesListComponent implements OnInit {
     return this.notes.filter(note => {
       return note.body.toLowerCase().includes(query) || note.title.toLowerCase().includes(query);
     });
+  }
+
+  sortByRelevancy = (searchResult: Note[]) => {
+    const noteCountObj: object = {};
+    searchResult.forEach(note => {
+      const noteId = this.notesService.getId(note);
+      if (noteCountObj[noteId]) {
+        noteCountObj[noteId] += 1;
+      } else {
+        noteCountObj[noteId] = 1;
+      }
+    });
+
+    this.filteredNotes = this.filteredNotes.sort((a: Note, b: Note) => {
+      const aId = this.notesService.getId(a);
+      const bId = this.notesService.getId(b);
+
+      const aCount = noteCountObj[aId];
+      const bCount = noteCountObj[bId];
+
+      return bCount - aCount;
+    });
+  }
+
+  checkNoteIsEmpty = () => {
+    this.noteCount = this.filteredNotes.length > 0;
+    console.log(this.noteCount);
+  }
+
+  generateLink = (note: Note) => {
+    return this.notesService.getId(note);
   }
 
 }
